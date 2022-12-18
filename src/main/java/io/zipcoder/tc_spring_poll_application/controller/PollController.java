350452c44227ab8e17971ca68c82dee9998167be
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-12-18 18:18:33 -0500

    spring boot working

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -5,15 +5,13 @@ import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PollController {
+    @Autowired
     PollRepository pollRepository;
 
-    @Autowired
     public PollController(PollRepository pollRepository) {
         this.pollRepository = pollRepository;
     }
--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/CrudRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/CrudRepository.java
@@ -1,7 +1,9 @@
 package io.zipcoder.tc_spring_poll_application.repositories;
 
+import io.zipcoder.tc_spring_poll_application.domain.Poll;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface CrudRepository<Option, Long> {
+    Iterable<Poll> findAll();
 }
